7ad767ca3bbab6a83ce9add09d694ac77ef55261
Couch-Guy <46738322+Couch-Guy@users.noreply.github.com>
2020-02-27 16:57:58 -0500

    Update Problem_3_Updated.py

--- Problem_3_Updated.py
+++ Problem_3_Updated.py
@@ -1,9 +1,9 @@
 #This is an update with reduced coding for easier intgration in future projects
 
-def factorization():
+def factorization():        #This is the main part of the code that finds all prime numbers
     p=n
     x=2
-    while p % x != 0:
+    while p % x != 0:       #This is used so that the next function can run and isn't skipped
         x += 1
     while p % x == 0:
         list_prime.append(x)
