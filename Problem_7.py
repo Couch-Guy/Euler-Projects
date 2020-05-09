84fb5bc541ad41ce3fc27ac5feb3ee95e3cd3bc0
Couch-Guy <46738322+Couch-Guy@users.noreply.github.com>
2020-05-09 03:14:15 -0400

    Update Problem_7.py
    Updated due to last code running past 1 minute. This new one should get the number in less than one second.

--- Problem_7.py
+++ Problem_7.py
@@ -1,19 +1,24 @@
-def factorization():
-    p=1
-    while len(list_prime)<n:
-        x = 2
-        p += 1
-        while x<=p:
-            if p==x:
-                list_prime.append(x)
+import math
+def prime_range():
+    p=11
+    while len(list_prime)<n+1:
+        true = 0
+        x = math.ceil(math.sqrt(p))
+        c=2
+        while c<=x:
+            if p%c==0:
+                true=1
                 break
-            if p%x==0:
-                break
-            x += 1
+            c+=1
+        if true==0:
+            list_prime.append(p)
+        p+=1
 
 print("What is your number?")
 n=eval(input())
-list_prime=[]
-factorization()
+list_prime=[1,2,3,5,7]          ###To speed up process###
+prime_range()
 print(list_prime)
-print("The",n,"prime number is", list_prime[n-1])
+print("The",n,"prime number is", list_prime[n])
+
+
